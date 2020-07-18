4a8002a5b2aa7751a1b74d35ed850e6a432bd7af
LakhanGandhi <33785683+LakhanGandhi@users.noreply.github.com>
2020-07-19 00:00:23 +0530

    Browser
    files sharing app test 001

---
+++ app/src/main/java/com/example/testdena/GlobalApplication.java
@@ -0,0 +1,22 @@
+package com.example.testdena;
+
+
+import android.app.Application;
+import android.content.Context;
+
+public class GlobalApplication extends Application {
+    private static Context GlobalContext;
+
+    @Override
+    public void onCreate() {
+        // TODO Auto-generated method stub
+        super.onCreate();
+        if(GlobalApplication.GlobalContext == null){
+            GlobalApplication.GlobalContext = getApplicationContext();
+        }
+    }
+
+    public static Context getGlobalAppContext() {
+        return GlobalApplication.GlobalContext;
+    }
+}
---
+++ app/src/main/java/com/example/testdena/ui/DeviceDetailFragment.java
@@ -0,0 +1,4 @@
+package com.example.testdena.ui;
+
+public class DeviceDetailFragment  {
+}
